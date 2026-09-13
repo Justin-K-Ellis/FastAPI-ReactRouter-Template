46a99723655b0bbd732fce7fbc8c7e6665b95aab
Justin-K-Ellis <jlke45@gmail.com>
2026-09-13 21:45:26 +0900

    feat: add starter Navbar component

---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,10 @@
+export default function Navbar() {
+  return (
+    <nav className="navbar bg-base-100 shadow">
+      <div className="navbar-start">
+        <p>Navbar</p>
+      </div>
+      <div className="navbar-end"></div>
+    </nav>
+  );
+}
--- client/src/layouts/RootLayout.tsx
+++ client/src/layouts/RootLayout.tsx
@@ -1,9 +1,10 @@
 import { Outlet } from "react-router";
+import Navbar from "../components/Navbar";
 
 export default function RootLayout() {
   return (
     <>
-      <nav>navbar</nav>
+      <Navbar />
       <main className="w-1/2 md:w-1/2 mx-auto">
         <Outlet />
       </main>
